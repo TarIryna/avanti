c6ad7f338943746401b674e7e9bd48973b7358b9
irynatar <tar86irina@gmail.com>
2026-01-30 15:42:47 +0200

    facebook 2

--- src/app/facebook.xml/route.js
+++ src/app/facebook.xml/route.js
@@ -12,7 +12,6 @@ export async function GET() {
       { cache: "no-store" }
     );// твои товары,
   const data =  await res.json();
-  console.log('length', data?.products?.length)
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0" xmlns:g="http://base.google.com/ns/1.0">
 <channel>
@@ -29,8 +28,9 @@ ${data?.products?.map(
   <g:link>https://avanti-shoes.com.ua/product/${p.code}</g:link>
   <g:image_link>${p.image1 ?? p.image ?? p.image2 ?? p.image3 ?? p.small_image}</g:image_link>
   <g:price>${p.price} UAH</g:price>
-  <g:gender>${p.gender}</g:gender>
+  <g:gender>${p.gender === 'boys' ? 'male' : p.gender === 'girls' ? 'female' : p.gender === 'women' ? 'female' : 'male'}</g:gender>
   <g:availability>in stock</g:availability>
+  <g:age_group>${p.gender === 'boys' || p.gender === 'girls'? 'kids' : 'adult'}</g:age_group>
   <g:condition>new</g:condition>
   <g:brand>Avanti</g:brand>
   <g:google_product_category><![CDATA[${ p.facebook || "Clothing & Accessories > Shoes & Footwear > Shoes"}]]></g:google_product_category>
