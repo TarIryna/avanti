992735207c857f8c20e9f627f284b09141e941ca
irynatar <tar86irina@gmail.com>
2026-01-30 15:29:10 +0200

    fix facebook

--- src/app/facebook.xml/route.js
+++ src/app/facebook.xml/route.js
@@ -2,12 +2,17 @@ import { NextResponse } from "next/server";
 import { fetchProductsByParams } from "@/helpers/useFetchProducts";
 
 export async function GET() {
-  const queryString = {limit: 3000, page: 1, gender: 'all'}?.toString()
+  const queryString = new URLSearchParams({
+    limit: 3000,
+    page: 1,
+    gender: 'all',
+  }).toString();
   const res = await fetch(
       `https://avanti-shoes.com.ua/api/products/filter?${queryString}`,
       { cache: "no-store" }
     );// твои товары,
   const data =  await res.json();
+  console.log('length', data?.products?.length)
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0" xmlns:g="http://base.google.com/ns/1.0">
 <channel>
@@ -24,6 +29,7 @@ ${data?.products?.map(
   <g:link>https://avanti-shoes.com.ua/product/${p.code}</g:link>
   <g:image_link>${p.image1 ?? p.image ?? p.image2 ?? p.image3 ?? p.small_image}</g:image_link>
   <g:price>${p.price} UAH</g:price>
+  <g:gender>${p.gender}</g:gender>
   <g:availability>in stock</g:availability>
   <g:condition>new</g:condition>
   <g:brand>Avanti</g:brand>
